ec1871b76dca68321254d6d6060c7d86ec809f90
Guanqi He <heguanqihgq@gmail.com>
2023-10-30 21:01:54 -0400

    solve DAE use assimulo

--- env/box1d.py
+++ env/box1d.py
@@ -1,50 +1,61 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
+from assimulo.problem import Implicit_Problem
+from assimulo.solvers import IDA
 import sympy as sym
+import matplotlib.pyplot as plt
 
-# Define the symbols and variables
-m, g, t = sym.symbols('m g t')
-x, v = sym.symbols('x v', cls=sym.Function)
+m,g,t = sym.symbols('m g t')
+x,v = sym.symbols('x v', cls=sym.Function)
 x = x(t)
-v = sym.diff(x, t)
+v = sym.diff(x,t)
 k = sym.symbols('k', real=True, positive=True)
 
 # Define the Lagrangian function
-L = sym.Rational(1, 2) * m * v**2 - m * g * x - sym.Rational(1, 2) * k * x**2
+L = sym.Rational(1,2)*m*v**2 - m*g*x - sym.Rational(1,2)*k*x**2
 
 # Use the Euler-Lagrange equation to obtain the differential equation of motion
-dL_dx = sym.diff(L, x)
-dL_dv = sym.diff(L, v)
-dL_dv_dt = sym.diff(dL_dv, t)
+dL_dx = sym.diff(L,x)
+dL_dv = sym.diff(L,v)
+dL_dv_dt = sym.diff(dL_dv,t)
 euler_lagrange_eq = dL_dv_dt - dL_dx
 print("Euler-Lagrange equation:", euler_lagrange_eq)
 
-x_dd = sym.solve(euler_lagrange_eq, sym.diff(v, t))[0]
+x_dd = sym.solve(euler_lagrange_eq, sym.diff(v,t))[0]
 print("Equilibrium points:", x_dd)
 
-x_dd = x_dd.subs([(k, 1), (m, 1), (g, 9.81)])
+x_dd = x_dd.subs([(k,1), (m,1), (g,9.81)])
 
 print(x_dd)
 
-x_dd = sym.lambdify([t, x, v], x_dd)
+x_dd = sym.lambdify([t,x,v], x_dd, 'numpy')
 
 # Define the function for the right-hand side of the differential equation
-def box1d_dynamics(t, y):
+def box1d_dynamics_residual(t, y, yd):
     x, v = y
-    dx_dt = v
-    dv_dt = x_dd(t, x, v)
-    return [dx_dt, dv_dt]
+    dx, dv = yd
+    res0 = dx - v
+    res1 = dv - x_dd(t, x, v)
+    return np.array([res0, res1])
 
 # Set the initial conditions and time span
 y0 = [0, 0]
-t_span = [0, 10]
+dy0 = [0, 0]
+t0 = 0.0
 
-# # Solve the differential equation numerically using solve_ivp
-sol = solve_ivp(box1d_dynamics, t_span, y0, t_eval=np.linspace(t_span[0], t_span[1], 1000))
+model = Implicit_Problem(box1d_dynamics_residual, y0, dy0, t0)
+model.name = 'Box1D'
 
-# # Plot the result
-plt.plot(sol.t, sol.y[0])
+sim = IDA(model)
+
+tfinal = 10
+ncp = 500
+
+t, y, yd = sim.simulate(tfinal, ncp)
+
+# Plot the result
+plt.plot(t, y[:,0], label='Position')
+plt.plot(t, y[:,1], label='Velocity')
+plt.legend()
+plt.ylabel('Position (m) / Velocity (m/s)')
 plt.xlabel('Time (s)')
-plt.ylabel('Position (m)')
 plt.show()
--- env/test.py
+++ env/test.py
@@ -1,37 +1,48 @@
-from sympy import symbols, lambdify
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+"""
+Tutorial example showing how to use the implicit solver IDA. To run the example
+simply type,
+
+    run tutorialIDA.py (in IPython)
+    
+or,
+
+    python tutorialIDA.py (in a command prompt)
+"""
 import numpy as np
-import scipy.integrate
-import matplotlib.pyplot as plt
-# Create symbols y0, y1, and y2
-y = symbols('y:3')
-kf, kb = symbols('kf kb')
-rf = kf * y[0]**2 * y[1]
-rb = kb * y[2]**2
-# Derivative of the function y(t); values for the three chemical species
-# for input values y, kf, and kb
-ydot = [2*(rb - rf), rb - rf, 2*(rf - rb)]
-ydot
-t = symbols('t') # not used in this case
-# Convert the SymPy symbolic expression for ydot into a form that
-# SciPy can evaluate numerically, f
-f = lambdify((t, y, kf, kb), ydot)
-k_vals = np.array([0.42, 0.17]) # arbitrary in this case
-y0 = [1, 1, 0] # initial condition (initial values)
-t_eval = np.linspace(0, 10, 50) # evaluate integral from t = 0-10 for 50 points
-# Call SciPy's ODE initial value problem solver solve_ivp by passing it
-#   the function f,
-#   the interval of integration,
-#   the initial state, and
-#   the arguments to pass to the function f
-solution = scipy.integrate.solve_ivp(f, (0, 10), y0, t_eval=t_eval, args=k_vals)
-# Extract the y (concentration) values from SciPy solution result
-y = solution.y
-# Plot the result graphically using matplotlib
-plt.plot(t_eval, y.T) 
-# Add title, legend, and axis labels to the plot
-plt.title('Chemical Kinetics') 
-plt.legend(['NO', 'Br$_2$', 'NOBr'], shadow=True) 
-plt.xlabel('time') 
-plt.ylabel('concentration') 
-# Finally, display the annotated plot
-plt.show()
+from assimulo.problem import Implicit_Problem #Imports the problem formulation from Assimulo
+from assimulo.solvers import IDA              #Imports the solver IDA from Assimulo
+
+
+def run_example():
+
+    def residual(t,y,yd):
+
+        res_0 = yd[0]-y[2]
+        res_1 = yd[1]-y[3]
+        res_2 = yd[2]+y[4]*y[0]
+        res_3 = yd[3]+y[4]*y[1]+9.82
+        res_4 = y[2]**2+y[3]**2-y[4]*(y[0]**2+y[1]**2)-y[1]*9.82
+
+        return np.array([res_0,res_1,res_2,res_3,res_4])
+    
+    #The initial conditons
+    t0  = 0.0 #Initial time
+    y0  = [1.0, 0.0, 0.0, 0.0, 0.0] #Initial conditions
+    yd0 = [0.0, 0.0, 0.0, -9.82, 0.0] #Initial conditions
+
+    model = Implicit_Problem(residual, y0, yd0, t0)             #Create an Assimulo problem
+    model.name = 'Pendulum'        #Specifies the name of problem (optional)
+
+    sim = IDA(model) #Create the IDA solver
+        
+    tfinal = 10.0        #Specify the final time
+    ncp = 500            #Number of communcation points (number of return points)
+
+    t,y,yd = sim.simulate(tfinal, ncp) #Use the .simulate method to simulate and provide the final time and ncp (optional)
+    
+    sim.plot()
+
+if __name__=='__main__':
+    run_example()
